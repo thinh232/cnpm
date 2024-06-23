6f128c419c33969cfc91489cda388b7190820de4
thinh232 <trankimthinh23@gmail.com>
2024-06-23 16:45:16 +0700

    test

---
+++ cheNgonNhatTheGioi.Module/BusinessObjects/classCheCode/SanPham.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace cheNgonNhatTheGioi.Module.BusinessObjects.classChe
+{
+    [DefaultClassOptions]
+    [DefaultProperty("tensp")]
+    public partial class SanPham
+    {
+        public SanPham(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
