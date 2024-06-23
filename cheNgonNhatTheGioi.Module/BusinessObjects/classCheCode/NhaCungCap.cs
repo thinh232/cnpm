5e730f1efadaae855bb5bd628615eadb5206acaa
thinh232 <trankimthinh23@gmail.com>
2024-06-23 16:28:33 +0700

    test

---
+++ cheNgonNhatTheGioi.Module/BusinessObjects/classCheCode/NhaCungCap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace cheNgonNhatTheGioi.Module.BusinessObjects.classChe
+{
+    [DefaultClassOptions]
+    [DefaultProperty("tenncc")]
+    public partial class NhaCungCap
+    {
+        public NhaCungCap(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
---
+++ cheNgonNhatTheGioi.Module/BusinessObjects/classCheCode/ThanhToan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace cheNgonNhatTheGioi.Module.BusinessObjects.classChe
+{
+    [DefaultClassOptions]
+    public partial class ThanhToan
+    {
+        public ThanhToan(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
